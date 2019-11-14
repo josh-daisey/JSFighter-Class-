1e57da770f551b807f36e3f3b68becc22f853b3f
Joshua Daisey <Joshu2020281@live.ytech.edu>
2019-11-14 11:12:28 -0500

    Shows controls

--- script/index.js
+++ script/index.js
@@ -1,4 +1,3 @@
-
 const START_HP = 20;
 const START_SP = 10;
 const DEFAULT_ATK = 5;
@@ -24,48 +23,56 @@ let controlsBox;
 let outputBox;
 
 class Fighter {
-  constructor(name, id) {
-    //'contructor' is in all JS classes
-    // It gets run immediately when a new object is created from a class
+    constructor(name, charaName) {
+        //'contructor' is in all JS classes
+        // It gets run immediately when a new object is created from a class
 
-    // Set all of our default values for this new fighter here
-    this.name = name;
-    this.hp = START_HP;
-    this.sp = START_SP;
-    this.atk = DEFAULT_ATK;
-    this.def = DEFAULT_DEF;
-    this.tek = DEFAULT_TEK;
-    this.id = id;
-  }
-  attack(target) {
-    console.log(this.name + ' attacked ' + target.name)
-  }
-  single(target) {
-    this.attack(target);
-  }
-  double(target) {
-    this.attack(target);
-    this.attack(target);
-  }
-  recover() {
-    console.log('Recovered!')
-  }
+        // Set all of our default values for this new fighter here
+        this.name = name;
+        this.hp = START_HP;
+        this.sp = START_SP;
+        this.atk = DEFAULT_ATK;
+        this.def = DEFAULT_DEF;
+        this.tek = DEFAULT_TEK;
+        this.charaName = charaName;
+    }
+    attack(target) {
+        console.log(this.name + ' attacked ' + target.name)
+    }
+    single(target) {
+        this.attack(target);
+    }
+    double(target) {
+        this.attack(target);
+        this.attack(target);
+    }
+    recover() {
+        console.log('Recovered!')
+    }
 }
 
+
+function showControls() {
+    controlsBox.innerHTML = '<button onclick="' + ((playerTurn) ? 'Player1' : 'Player0') + '.single('+ ((!playerTurn) ? 'Player1' : 'Player0') +')">Single Attack</button>'
+}
+
+
 function startup() {
-  Player0 = new Fighter(P0NAME, P0CHARA);
-  Player1 = new Fighter(P1NAME, P1CHARA);
+    Player0 = new Fighter(P0NAME, P0CHARA);
+    Player1 = new Fighter(P1NAME, P1CHARA);
 
-  gameBox = document.getElementById('gameBox');
-  headerBox = document.getElementById('headerBox');
-  graphicsBox = document.getElementById('graphicsBox');
-  barsBox = document.getElementById('barsBox');
-  controlsBox = document.getElementById('controlsBox');
-  outputBox = document.getElementById('outputBox');
+    gameBox = document.getElementById('gameBox');
+    headerBox = document.getElementById('headerBox');
+    graphicsBox = document.getElementById('graphicsBox');
+    barsBox = document.getElementById('barsBox');
+    controlsBox = document.getElementById('controlsBox');
+    outputBox = document.getElementById('outputBox');
 
-  graphicsBox.innerHTML = '<img id ="' + Player0.charaName + '" src="img/' + Player0.charaName + '_idle.png" alt="' + Player0.name + '" class="fighterIMG">'
-  graphicsBox.innerHTML += '<img id ="' + Player1.charaName + '" src="img/' + Player1.charaName + '_idle.png" alt="' + Player1.name + '" class="fighterIMG">'
+    graphicsBox.innerHTML = '<img id ="' + Player0.charaName + '" src="img/' + Player0.charaName + '_idle.png" alt="' + Player0.name + '" class="fighterIMG">'
+    graphicsBox.innerHTML += '<img id ="' + Player1.charaName + '" src="img/' + Player1.charaName + '_idle.png" alt="' + Player1.name + '" class="fighterIMG">'
 
-  console.log("My name is " + Player0.name + " and my ATK is " + Player0.atk)
-  console.log("My name is " + Player1.name + " and my ATK is " + Player1.atk)
+    showControls();
+
+    console.log("My name is " + Player0.name + " and my ATK is " + Player0.atk)
+    console.log("My name is " + Player1.name + " and my ATK is " + Player1.atk)
 }
